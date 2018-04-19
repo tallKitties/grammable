8282d3cdcd1b9ae0a50a80213afa20140184cb6b
Ben Kautt <bkautt73@gmail.com>
2018-04-19 09:49:27 -0700

    grams#new / #create, setup devise, validate message params

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,24 @@
 class GramsController < ApplicationController
+
   def index
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @book = Gram.new(gram_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 end
