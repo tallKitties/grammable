b80220b06322718f90819c81bd4c80668838b5d8
tallKitties <bkautt73@gmail.com>
2018-04-21 02:16:48 +0000

    grams#edit, grams#update actions & specs

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,13 +1,13 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_gram, only: [:show, :edit, :update]
 
   def index
   end
 
   def show
-    @gram = Gram.find_by_id(params[:id])
-    render :new, status: :not_found unless @gram
-  end
+    render_not_found unless @gram
+    end
 
   def new
     @gram = Gram.new
@@ -22,9 +22,31 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    render_not_found unless @gram
+  end
+
+  def update
+    return render_not_found unless @gram
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def set_gram
+    @gram = Gram.find_by_id(params[:id])
+  end
+
+  def render_not_found
+    render plain: 'Not Found', status: :not_found
   end
 end
